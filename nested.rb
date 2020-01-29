544952affa3b5b4e78fcc3853a652d88df07cfe9
Micah Banschick <micha.ban@gmail.com>
2020-01-29 16:16:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  return programmer_hash[:dennis_ritchie][:languages]
+  return programmer_hash
 end
