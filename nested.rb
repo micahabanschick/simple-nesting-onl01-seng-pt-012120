7be3f0df82e2588060149476a096c97e841161b7
Micah Banschick <micha.ban@gmail.com>
2020-01-29 16:06:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     return programmer_hash[:alan_kay][:known_for][0]
+     return programmer_hash[:alan_kay][:known_for]
      
 end
 
@@ -125,5 +125,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  return programmer_hash[:dennis_ritchie][:languages][0]
+  return programmer_hash[:dennis_ritchie][:languages]
 end
